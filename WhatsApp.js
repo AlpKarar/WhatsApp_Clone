c3ab80d55892fe3fd61fd39bff39e107e9a1de50
Alp Karar <88462979+AlpKarar@users.noreply.github.com>
2022-04-22 14:10:31 +0300

    Update WhatsApp.js

--- WhatsApp.js
+++ WhatsApp.js
@@ -5,15 +5,7 @@ let isOpenOptionMain = false;
 optionMain.addEventListener("click", function() {
     isOpenOptionMain = true;
     let container = document.querySelector(".relative");
-    container.classList.remove("hidden");
-    console.log(isOpenOptionMain);    
+    container.classList.remove("hidden");       
 })
-
-
-let body = document.querySelector("body");
-
-body.addEventListener("click", () => isOpenOptionMain = false);
-
-console.log(body);
 
 
